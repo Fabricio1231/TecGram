c0ab6205b3cc2ec6b1be9176e46765c560886e44
Fabricio <NTVFPCesar@gmail.com>
2023-05-05 13:59:39 -0600

    commit-2

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -4,24 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Illuminate\Image\Facades\Image;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $imagen = $request->file('file');
 
-        $nombreImagen = Str::uuid(). "." . 
-        $imagen->extension();
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
         
         $imagenServidor = Image::make($imagen);
-        $imagenServiidor->fit(500,500);
+        $imagenServidor->fit(500, 500);
         
-        $imagenRuta = public_path('uploads') . '/' . 
-        $nombreImagen;
+        $imagenRuta = public_path('uploads') . '/' . $nombreImagen;
         
         $imagenServidor->save($imagenRuta);
 
-        return response()->json(['imagen'=>$nombreImagen]);
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
